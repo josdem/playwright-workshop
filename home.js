90d8586185024e32b383e19ac973f204e03fbd1f
josdem <joseluis.delacruz@gmail.com>
2023-06-07 08:47:22 -0400

    Adding slow motion constant

--- home.js
+++ home.js
@@ -1,7 +1,8 @@
 const {chromium} = require('playwright');
+const SMALL_SLOW_MOTION = 50;
 
 (async () => {
-    const browser = await chromium.launch({headless: false, slowMo: 50})
+    const browser = await chromium.launch({headless: false, slowMo: SMALL_SLOW_MOTION})
     const page = await browser.newPage()
     await page.goto('https://vetlog.org/')
     await browser.close()
