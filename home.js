741cbc6779de2052dff75046d2d89efc664f181d
josdem <joseluis.delacruz@gmail.com>
2023-06-07 08:17:28 -0400

    Adding first test

---
+++ home.js
@@ -0,0 +1,8 @@
+const {chromium} = require('playwright')
+
+(async () => {
+    const browser = await chromium.launch({headless: false, slowMo: 50})
+    const page = await browser.newPage()
+    await page.goto('https://vetlog.org/')
+    await browser.close()
+})()
