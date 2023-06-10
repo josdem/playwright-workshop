c90549a77ae3b24cd26f08d59fb2436e8e4a54ef
josdem <joseluis.delacruz@gmail.com>
2023-06-10 18:48:57 -0400

    #1 Adding Applitools configuration

---
+++ utils/applitools.util.js
@@ -0,0 +1,24 @@
+import {
+  BatchInfo,
+  VisualGridRunner,
+  BrowserType,
+  DeviceName,
+  ScreenOrientation,
+} from '@applitools/eyes-playwright'
+
+let configuration
+let runner
+
+const setUpConfiguration = async (batchName) => {
+    runner = new VisualGridRunner({ testConcurrency: 5 })
+  
+    configuration = eyes.getConfiguration()
+    configuration.setBatch(new BatchInfo(batchName))
+    configuration.addBrowser(CHROME.width, CHROME.height, BrowserType.CHROME)
+    configuration.addBrowser(FIREFOX.width, FIREFOX.height, BrowserType.FIREFOX)
+    configuration.addBrowser(EDGE.width, EDGE.height, BrowserType.EDGE_CHROMIUM)
+    configuration.addBrowser(SAFARI.width, SAFARI.height, BrowserType.SAFARI)
+    configuration.addDeviceEmulation(DeviceName.iPhone_11, ScreenOrientation.PORTRAIT)
+    configuration.addDeviceEmulation(DeviceName.Pixel_5, ScreenOrientation.PORTRAIT)
+}
+
