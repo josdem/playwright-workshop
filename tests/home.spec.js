d128f3af330a8007de1affa9def21dc2c4bfcffe
josdem <joseluis.delacruz@gmail.com>
2024-02-18 14:03:52 -0500

    #10 Adding custom tag with US

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,6 +1,17 @@
 const { test, expect } = require("@playwright/test")
 const properties = require("../properties/test.properties")
 
+test.beforeAll(async () => {
+  test.info().annotations.push({
+    type: 'story',
+    description: 'https://github.com/josdem/playwright-workshop/issues/10',
+  })
+  test.info().annotations.push({
+    type: 'time',
+    description: `${new Date()}`,
+  })
+})
+
 test("should validate page title", async ({ page }) => {
   await page.goto(properties.url)
   await expect(page).toHaveTitle(properties.title)
