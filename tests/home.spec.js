c09b7c1de0f2462d43d34950acdfc2917af2ce6c
josdem <joseluis.delacruz@gmail.com>
2024-02-28 09:27:49 -0500

    Formatting code

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -4,19 +4,19 @@ const properties = require("../properties/test.properties")
 
 test.beforeAll(async () => {
   test.info().annotations.push({
-    type: 'story',
-    description: 'https://github.com/josdem/playwright-workshop/issues/10',
+    type: "story",
+    description: "https://github.com/josdem/playwright-workshop/issues/10",
   })
   test.info().annotations.push({
-    type: 'time',
+    type: "time",
     description: `${new Date()}`,
   })
 })
 
 test("should validate page title", async ({ page }) => {
   await allure.tags("home", "guest", "title")
-  await allure.epic("OR-2716");
-  await allure.story("OR-4197");
+  await allure.epic("OR-2716")
+  await allure.story("OR-4197")
   await allure.issue("tags", "https://github.com/josdem/playwright-workshop/issues/12")
   await page.goto(properties.url)
   await expect(page).toHaveTitle(properties.title)
